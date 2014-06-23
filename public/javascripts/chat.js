b09984da9a682773f297b06c6eaa0a8635e0ce64
Will+Ari <Will+Ari@appacademy.io>
2014-06-23 18:05:36 -0400

    Finish CSS

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,10 +3,21 @@
   
   var Chat = ChatApp.Chat = function(socket) {
     this.socket = socket;
+    this.room = "lobby";
   };
   
   Chat.prototype.sendMessage = function(text) {
     this.socket.emit('message', { text: text });
+  };
+  
+  Chat.prototype.changeName = function(name) {
+    this.socket.emit('nicknameChangeRequest', { nickname: name });
+  };
+  
+  Chat.prototype.changeRoom = function(room) {
+    this.room = room;
+    this.socket.emit('roomChangeRequest', room);
+    this.sendMessage("Switched to " + room);
   };
   
 })(this);
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -4,14 +4,33 @@
   
   
   var getInput = function(chat, text){
-    chat.sendMessage(text);
+    if (text[0] === "/") {
+      var command = text.split(" ")[0].slice(1);
+      var request = text.split(" ")[1];
+      switch (command){
+        case "nick":
+          chat.changeName(request);
+          break;
+        case "join":
+          chat.changeRoom(request);
+          break;
+        default:
+          break;
+      };
+    } else {
+        chat.sendMessage(text);
+    }
   }
   
   $(document).ready(function() {
     var chat = new ChatApp.Chat(socket);
     
     socket.on("message", function(message){
-      $('#messages').append(message.text + "<br>");
+       $('#messages').append("<b>" + message.name + "</b>: " + message.text + "<br>"); 
+    });
+    
+    socket.on("nicknameChangeResult", function(nicknameChangeResult){
+      $('#messages').append("<i>name changed to " + nicknameChangeResult.nickname + "</i><br>");
     });
     
     $('#submit-form').submit(function(event){
